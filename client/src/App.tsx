d62ad2dcaf013b209edefcb3f0aba66efff2d75e
rcontrol-factory <tevesjulio16@gmail.com>
2026-02-15 05:13:39 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster";
 import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
